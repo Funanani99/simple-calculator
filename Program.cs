0ed076ee50be07e506d9e625bac71a858b73c553
Dell-User <Dell-User@DESKTOP-GCMBBUK>
2023-10-12 19:34:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace simple_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter First number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second number");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(num1 + "+" + num2 + " = " + (num1 + num2));
+            Console.WriteLine(num1 + "-" + num2 + " = " + (num1 - num2));
+            Console.WriteLine(num1 + "*" + num2 + " = " + (num1 * num2));
+            Console.WriteLine(num1 + "/" + num2 + " = " + (num1 / num2));
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadLine();
+
+        }
+    }
+}
